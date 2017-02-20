a35b01580b4b02fab16f3e32acee4f544658502e
Maythee Anegboonlap <null@llun.me>
2017-02-20 11:19:18 +0800

    Add prettier-standard-format

--- jsprettier.js
+++ jsprettier.js
@@ -1,20 +1,7 @@
-'use strict';
-var getStdin = require('get-stdin');
-var prettier = require('prettier');
-var extend = require('extend');
+'use strict'
+const getStdin = require('get-stdin')
+var prettierStandard = require('prettier-standard')
 
-getStdin().then(function(data) {
-  var scope = process.argv[3];
-  var opts;
-  var js;
-  // use the settings from .sublime-settings
-  opts = extend({}, opts, JSON.parse(process.argv[2]));
-  try {
-    if (scope) {
-      js = prettier.format(data, opts);
-    }
-    process.stdout.write(js);
-  } catch (err) {
-    throw err;
-  }
-});
+getStdin().then(function (data) {
+  prettierStandard.format(data).then(process.stdout)
+})
