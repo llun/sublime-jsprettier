ddfc8da5855fab6b2db009d97bcca8b32133654e
Maythee Anegboonlap <null@llun.me>
2017-02-20 11:52:13 +0800

    Fix error

--- jsprettier.js
+++ jsprettier.js
@@ -1,7 +1,16 @@
 'use strict'
-const getStdin = require('get-stdin')
-var prettierStandard = require('prettier-standard')
+var getStdin = require('get-stdin')
+var prettierStandard = require('prettier-standard-formatter')
 
 getStdin().then(function (data) {
-  prettierStandard.format(data).then(process.stdout)
+  var scope = process.argv[3]
+  try {
+    if (scope) {
+      prettierStandard.format(data).then(result => {
+        process.stdout.write(result)
+      })
+    }
+  } catch (error) {
+    throw error
+  }
 })
